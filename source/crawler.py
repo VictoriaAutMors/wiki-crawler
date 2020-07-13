e7041372ebbbd2f901ba00c8d03c2e9daa5103fb
Timur Alikhanov <55821089+VictoriaAutMors@users.noreply.github.com>
2020-07-13 16:06:22 +0600

    updated crawler

--- source/crawler.py
+++ source/crawler.py
@@ -1,47 +1,85 @@
-import httplib2
-import bs4 as bs
-from time import sleep
 from collections import deque
+import sys
+from urllib.parse import urljoin, unquote
+import requests
 from urllib.request import urlopen
-import collections
-from collections import defaultdict
+import re
+import multiprocessing as mp
+import requests
+import re
+import urllib.request, urllib.error, urllib.parse
 
-def download_page(url):
-    page = urlopen(url)
-    html_content = page.read()
-    html_content = str(html_content)
-    file = open('pages/' + url + '.txt', 'w')
-    file.write(html_content)
-    file.close()
+urlTemplate = r"/wiki/%[a-zA-Z_\.0-9/%]+"
+baseURL = "https://kk.wikipedia.org"
 
-    urls_bfs = []    
-    
-def get_links_bfs(url):
-    urls = []
-    http = httplib2.Http()
-    sleep(0.1)
-    status, response = http.request(url)
-    soup = bs.BeautifulSoup(response, "html.parser")
-    for div in soup.find_all("div", {"class":"mw-body-content"}):
-        for link in div.select("a"):
-            if link.has_attr('href'):
-                if link['href'].startswith("/wiki/") and ":" not in link['href'] and "Main_Page" not in link['href'] and "#" not in link['href']: 
-                    urls.append(link['href'])
-    urls = ['https://en.wikipedia.org' + s for s in urls]
-    return urls
+def get_urls(url):
+    print("crawling " + unquote(url))
+    answer = requests.get(url=url)
+    urls = re.findall(urlTemplate, answer.text)
+    for url in urls:
+        yield baseURL + url
 
-def bfs(url):
-    queue = deque([(url,0)])
-    depth = 1
-    while queue and len(set(urls_bfs)) < 1000:
-        url,depth = queue.popleft()
-        if depth < 3:
-                download_page(url)
-                if url not in urls_bfs:
-                    urls_bfs.append(url)
-                links = get_links_bfs(url) 
-                for link in links:
-                    if link not in queue and link not in urls_bfs:
-                        queue.append((link, depth + 1))
+def print_report_for_lvl(crawled, max_url):
+    print("DEPTH IS REACHED!")
+    print("Crowled sites:", crawled)
+    print("Site with biggest quantity of symbols:")
+    answer = requests.get(max_url)
+    print(unquote(max_url) + str(" : ") + str(len(answer.text)))
 
-bfs('https://en.wikipedia.org/wiki/Main_Page')
+def visit_for_url(url, marked, queue, crawled, lvl, max_url):
+    for url in get_urls(url):
+        crawled += 1
+        if url in marked:
+            continue
+        marked.add(url)
+        queue.append(url)
+    return marked, queue, crawled
+
+def crawl_for_lvl(url, given_lvl):  
+    queue = deque([])
+    marked = set()
+    crawled = 0
+    queue.append(url)
+    marked.add(url)
+    lvl = 1
+    len_queue_old = 1
+    max_url = url
+    while queue:
+        url = queue.popleft()
+        len_queue_old -= 1
+        marked, queue, crawled = visit_for_url(url, marked, queue, crawled, lvl, max_url)
+        answer = requests.get(url)
+#	code for download crawled pages
+#	try:
+#            answer1 = urllib.request.urlopen(url)
+#            webContent = answer1.read()
+#            url_for_download = ""
+#            iterator = len(unquote(url)) - 1
+#            while(url[iterator] != "/"):
+#                url_for_download += (unquote(url)[iterator])
+#                iterator -= 1
+#            url_for_download = url_for_download[::-1]
+#            f = open('WikiKz' + str(url_for_download) + str(lvl) + '.html', "w+")
+#            f.write(str(webContent))
+#            f.close()
+#	except:
+#            pass 
+        answer_max_url = requests.get(max_url)
+        if (len(answer.text) > len(answer_max_url.text)):
+            max_url = url
+        if (len_queue_old == 0):
+            len_queue_old = len(queue)
+            lvl += 1
+        if (lvl == given_lvl):
+            print_report_for_lvl(crawled, max_url)
+            exit()
+        
+print("Enter the Depth of search:")
+depth = input()
+print("Enter the Start page or leave it empty to use welcome page")
+start_page = input()
+if not start_page:
+	start_page = baseURL
+crawl_for_lvl(start_page, depth)
+exit()
+
